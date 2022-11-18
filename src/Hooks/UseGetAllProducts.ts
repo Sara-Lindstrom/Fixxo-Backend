c9da524b5fc36e91e8e1137054d66c6a73829395
sara-lindstrom <sara.maria.lindstrom@hotmail.com>
2022-11-18 08:59:36 +0100

    transition fron Js to Ts complete!

--- src/Hooks/UseGetAllProducts.ts
+++ src/Hooks/UseGetAllProducts.ts
@@ -1,21 +1,44 @@
 import { useEffect, useState} from 'react';
+import IProduct from '../assets/models/IProduct';
 import ICartItem from '../assets/models/useShoppingContextModels/ICartItem';
 
 
 export const UseGetAllProducts = () => {
 
-    const [allProducts, setAllProducts] = useState<ICartItem[]|null>(null)
+    const [isReady, setIsReady] = useState<boolean>(false);
+    const [allProducts, setAllProducts] = useState<IProduct[]>([])
 
         useEffect (() => {
         const fetchAllData = async () => {
             let result = await fetch('https://win22-webapi.azurewebsites.net/api/products')
             setAllProducts(await result.json())
+            setIsReady(true)
         }
         fetchAllData()
 
-        }, [])
+    }, [])
+    
+    const [allCartItems, setAllCartItems] = useState<ICartItem[]>([])
 
-  return allProducts
+    // useEffect to create ICartItems from Api result
+    useEffect (()=> {
+        if (isReady === false){
+            return;
+        }
+
+        let cartItems = allProducts.map(p => {
+            let cartitem: ICartItem = {
+                quantity:0,
+                product:p
+            }
+            return cartitem;
+        })
+
+        setAllCartItems(cartItems)
+
+    },[isReady])
+
+  return allCartItems
 }
 
 export default UseGetAllProducts
